10f1de3fe3beb1a1b50975a6d2e92a933cbae606
VicBojoDev10 <reybojorgejr@hotmail.com>
2025-10-06 08:59:55 -0600

    flechaaaas

---
+++ Mecanica_1/Assets/Codigos/BowBehaviour.cs
@@ -0,0 +1,93 @@
+using UnityEngine;
+
+[RequireComponent(typeof(LineRenderer))]
+public class BowBehaviour : MonoBehaviour
+{
+    public Transform[] points;
+    public Transform shootPoint;
+    public GameObject arrowPrefab;
+
+    public float bowCompression;
+    public float arrowMaxSpeed;
+    public float arrowMaxDisplacement;
+    public float pullSpeed;
+    public float releaseSpeed;
+
+    public bool applyingTension;
+    private GameObject arrow;
+    // Start is called before the first frame update
+    void Start()
+    {
+        GetComponent<LineRenderer>().widthMultiplier = 0.05f;
+        GetComponent<LineRenderer>().positionCount = 3;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+       if(Input.GetMouseButtonDown(0))
+       {
+            applyingTension = true;
+            SetArrow();
+       }
+
+        if (applyingTension)
+            PullArrow();
+        else
+            ReleaseArrow();
+
+        if (Input.GetMouseButtonUp(0))
+        {
+            applyingTension = false;
+            FireArrow();
+        }
+
+        DrawChord();
+    }
+
+    void DrawChord()
+    {
+        Vector3[] pointPositions = new Vector3[] { points[0].position,
+                                                   points[1].position,
+                                                   points[2].position };
+        GetComponent<LineRenderer>().SetPositions(pointPositions);
+    }
+
+    void SetArrow()
+    {
+        arrow = Instantiate(arrowPrefab, shootPoint);
+        arrow.GetComponent<Rigidbody>().isKinematic = true;
+        arrow.GetComponent<TrailRenderer>().emitting = false;
+    }
+
+    void PullArrow()
+    {
+        float dt = Time.deltaTime;
+        Vector3 newPosition = new Vector3(0, 0, -arrowMaxDisplacement);
+        points[1].localPosition = Vector3.Lerp(points[1].localPosition, newPosition, pullSpeed * dt);
+
+        Vector3 newScale = new Vector3(bowCompression, 1, 1);
+        transform.localScale = Vector3.Lerp(transform.localScale, newScale, pullSpeed * dt);
+
+    }
+
+    void ReleaseArrow()
+    {
+        float dt = Time.deltaTime;
+        Vector3 newPosition = Vector3.zero;
+        points[1].localPosition = Vector3.Lerp(points[1].localPosition, newPosition, releaseSpeed * dt);
+
+        Vector3 newScale = Vector3.one;
+        transform.localScale = Vector3.Lerp(transform.localScale, newScale, releaseSpeed * dt);
+    }
+
+    void FireArrow()
+    {
+        float normArrowDisplacement = points[1].localPosition.magnitude / arrowMaxDisplacement;
+        arrow.GetComponent<Rigidbody>().isKinematic = false;
+        arrow.GetComponent<TrailRenderer>().emitting = true;
+        arrow.transform.parent = null;
+        arrow.GetComponent<Rigidbody>().velocity = arrowMaxSpeed * normArrowDisplacement * shootPoint.forward;
+        arrow.GetComponent<TrailRenderer>().emitting = false;
+    }
+}
